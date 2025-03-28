fb97134fd23b67132887e4023b0be6fb11518ec2
dilbazi <you@example.com>
2025-03-28 11:42:09 +0300

    add meta class in midels

--- orderscafe/myproject/orderscafeapp/models.py
+++ orderscafe/myproject/orderscafeapp/models.py
@@ -13,6 +13,12 @@ class Dish(BaseModel):
     name = models.CharField(max_length=255, verbose_name="dish name")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="price")
 
+    class Meta:
+
+        db_table = "dish"
+        verbose_name = "Dish"
+        verbose_name_plural = "Dishes"
+
     def __str__(self) -> str:
         return self.name + "-" + str(self.price)
 
@@ -25,6 +31,12 @@ class Orders(BaseModel):
         max_digits=10, decimal_places=2, verbose_name="total price", null=True, blank=True
     )
     status = models.CharField(max_length=2, choices=CHOICES, verbose_name="status", default="w")
+
+    class Meta:
+
+        db_table = "orders"
+        verbose_name = "Order"
+        verbose_name_plural = "Orders"
 
     def __str__(self) -> str:
         return str(self.id)
